99ac2f6fb0bb418d1705af864310cbc7e308ce28
Rey <cpcpcoole@gmail.com>
2024-01-22 18:58:36 +0900

    design: layout.css wrap-content-area max-width 수정

--- src/main/java/com/runner/ddida/controller/UserController.java
+++ src/main/java/com/runner/ddida/controller/UserController.java
@@ -2,8 +2,12 @@ package com.runner.ddida.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import lombok.RequiredArgsConstructor;
+
+/**
+* @author 박재용
+* @editDate 24.01.22 ~
+*/
 
 @Controller
 @RequiredArgsConstructor
