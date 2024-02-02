f755277c1dfc1c2695f3b6bddd361beb3107f869
aaaaawwwwww <147010771+aaaaawwwwww@users.noreply.github.com>
2024-02-02 18:28:23 +0900

    feat: 회원 목록, 회원 상세 데이터 표시

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,3 +1,15 @@
-insert into user(username, password, phone, email) values('hong', '{noop}1111', '010-1111-1111', 'hong@gmail.com');
-insert into user values(null, 'park@gmail.com', '{noop}1111', '010-1111-1111', 'park');
+insert into member(username, password, name, role, phone, email, sign_date) values('admin', '1', 'yg', 'ADMIN', '0', '0@0', '2024-01-31');
+insert into member(username, password, name, role, phone, email, sign_date) values('user', '1', 'yg', 'USER', '0', '0@0', '2024-02-01');
+insert into member(username, password, name, role, phone, email, sign_date) values('aaaaa', '1', 'son', 'USER', '0', '0@0', '2023-08-01');
+insert into member(username, password, name, role, phone, email, sign_date) values('bbbb', '1', 'mom', 'USER', '0', '0@0', '2020-10-05');
+insert into member(username, password, name, role, phone, email, sign_date) values('ccccc', '1', 'dad', 'USER', '0', '0@0', '2024-11-08');
+insert into member(username, password, name, role, phone, email, sign_date) values('dddd', '1', 'daughter', 'USER', '0', '0@0', '2022-12-12');
+insert into member(username, password, name, role, phone, email, sign_date) values('eeeeeee', '1', 'grandmother', 'USER', '0', '0@0', '2023-02-10');
+insert into member(username, password, name, role, phone, email, sign_date) values('fffffff', '1', 'father', 'USER', '0', '0@0', '2021-10-18');
+insert into member(username, password, name, role, phone, email, sign_date) values('gggg', '1', 'mother', 'USER', '0', '0@0', '2021-04-05');
+insert into member(username, password, name, role, phone, email, sign_date) values('hhhhh', '1', 'uncle', 'USER', '0', '0@0', '2017-05-31');
+insert into member(username, password, name, role, phone, email, sign_date) values('iiiii', '1', 'cousin', 'USER', '0', '0@0', '2021-02-01');
+insert into member(username, password, name, role, phone, email, sign_date) values('jjjjjj', '1', 'aaaaa', 'USER', '0', '0@0', '2020-07-20');
 
+select * from member;
+show tables;
