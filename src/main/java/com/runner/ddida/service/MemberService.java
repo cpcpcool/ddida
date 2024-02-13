60706d51636fff56cb9759c70ddd8b72733f5712
Rey <cpcpcoole@gmail.com>
2024-02-13 11:44:27 +0900

    design: background 수정

--- src/main/java/com/runner/ddida/service/MemberService.java
+++ src/main/java/com/runner/ddida/service/MemberService.java
@@ -38,52 +38,14 @@ public class MemberService {
 		return memberRepository.findByUserNo(userNo);
 	}
 
-	// 검색 페이징
-//	public Page<Member> searchUsers(String searchKeyword, String searchType, Pageable pageable) {
-//		if (searchKeyword == null || searchType == null) {
-//			return memberRepository.findAll(pageable);
-//		} else {
-//			switch (searchType) {
-//			case "username":
-//				return memberRepository.findByUsernameContaining(searchKeyword, pageable);
-//			case "name":
-//				return memberRepository.findByNameContaining(searchKeyword, pageable);
-//			case "role":
-//				return memberRepository.findByRoleContaining(searchKeyword, pageable);
-//			case "signDate":
-//				return memberRepository.findBySignDateContaining(searchKeyword, pageable);
-//			default:
-//				return memberRepository.findAll(pageable);
-//			}
-//		}
-//	}
-
 	public Map<String, Object> searchUsers(String searchKeyword, String searchType, Pageable pageable) {
 		Map<String, Object> result = new HashMap<>();
 		List<Long> userNoList = memberRepository.getUserNoList();
 		List<Object[]> qnaCounts = qnaRepository.count(userNoList);
 		result.put("qnaCounts", qnaCounts);
-		System.out.println("===================================");
-		for(Object[] row: qnaCounts) {
-			String username = (String)row[0];
-			Long qnaCount = (Long)row[1];
-			System.out.println("아이디 : " + username);
-			System.out.println("작성 문의글 수 : " + qnaCount);
-		}
-		System.out.println("===================================");
 		
 		List<Long[]> userStatistics = reserveRepository.getUserStatistics(userNoList);
 		result.put("userStats", userStatistics);
-		for(Long[] row: userStatistics) {
-			Long userNo = row[0];
-			Long reserveCount = row[1];
-			Long reviewCount = row[2];
-			System.out.println("사용자고유번호 :" + userNo);
-			System.out.println("예약횟수 : " + reserveCount);
-			System.out.println("작성 후기 수 : " + reviewCount);
-			System.out.println();
-		}
-		
 		if (searchKeyword == null || searchType == null) {
 			result.put("result", memberRepository.findAll(pageable));
 		} else {
